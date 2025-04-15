589fbca61cea79d160d0c9ea70f10159e5384dfa
CeciliaSalinas <salinascecilia.mdl@gmail.com>
2025-04-15 10:15:05 -0300

    introduccion a Js

---
+++ Clase02/js/app.js
@@ -0,0 +1,52 @@
+/* document.write('Hola mundo')
+
+// Tipos de datos
+"Hola mundo" //string
+
+// Númenros
+10000
+-2.3
+
+// Booleanos
+true
+false
+
+//array
+['juan', 'roberto', 'ana']
+[1,2,3]
+[true, false, false, true, true]
+
+// Object
+{
+    "username": 'Roberto'
+    "score": 70.4
+    "hours": 14
+    "professional": true
+}
+
+{
+    "username": 'Roberto'
+    "score": 0.4
+    "hours": 2
+    "professional": true
+}
+ */
+
+// Variables
+var nombreDelUsuario = "Carlos"
+let apellido = "Marquez"
+
+let nombreCompleto = nombreDelUsuario + apellido
+
+console.log(nombreCompleto)
+console.log(nombreDelUsuario, apellido)
+
+const PI = 3.1415
+console.log(PI)
+
+let numeroUno = 60
+let numeroDos = 120
+
+let resultado = numeroUno + numeroDos
+console.log(resultado)
+
