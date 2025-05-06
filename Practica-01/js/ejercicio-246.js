cfe43f139b32f37947bf8354a315c0985e374086
CeciliaSalinas <91616144+CeciliaSalinas@users.noreply.github.com>
2025-05-06 10:12:03 -0300

    nueva practica js

---
+++ Practica-01/js/ejercicio-246.js
@@ -0,0 +1,44 @@
+console.log('Practica Javascript 01')
+
+
+const titulo = document.createElement('h1')
+titulo.textContent ='Tabla de temperatura Fahrenheit a Celsius'
+
+const tabla = document.createElement('table')
+tabla.id = 'tabla'
+
+const encabezado = document.createElement('tr')
+const tituloFar = document.createElement('th')
+tituloFar.textContent = 'Fahrenheit'
+const tituloCel = document.createElement('th')
+tituloCel.textContent = 'Celsius'
+
+
+
+encabezado.appendChild(tituloFar)
+encabezado.appendChild(tituloCel)
+tabla.appendChild(encabezado)
+
+
+
+/* -----BUCLE FOR PARA RECORRER E IMPRIMIR TABLA----- */
+for( let far = 1; far <= 65; far ++ ){
+    const convierteCelsius = 5/9 * (far - 32) 
+
+    const fila = document.createElement ('tr')
+    const celdaFar = document.createElement('td')
+    const celdaCel = document.createElement ('td')
+
+    celdaFar.textContent = far
+    celdaCel.textContent = convierteCelsius
+
+    fila.appendChild(celdaFar)
+    fila.appendChild(celdaCel)
+    tabla.appendChild(fila)
+}
+
+/* INYECTAR EN EL MAIN */
+
+let main = document.querySelector('main') 
+main.appendChild(titulo) 
+main.appendChild(tabla) 
