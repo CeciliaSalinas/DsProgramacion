debecac4528f7e5ddc059e68efd2f4ae101c2c3c
CeciliaSalinas <salinascecilia.mdl@gmail.com>
2025-05-13 11:51:14 -0300

    resolucion ejercicio con switch

--- Practica-02/ejercicio-326.js
+++ Practica-02/ejercicio-326.js
@@ -1,42 +1,48 @@
 console.log('Practica Javascript 02 - E334')
 
-/* -------pido al usuario el ingreso de datos----------- */
+
 let ingresaValor = prompt('Ingresá un valor en números:')
 let eligeUnidad = prompt('Escribir la unidad de medida a convertir (kl, hl, dal, l, dl, cl, ml):')
 
-// Convertimos el valor a litros primero
-let valorUnidadLitro;
 
-if (eligeUnidad === 'kl') {
+let valorUnidadLitro = 0
+
+switch (eligeUnidad) {
+  case 'kl':
     valorUnidadLitro = ingresaValor * 1000
-} else if (eligeUnidad === 'hl') {
+    break
+  case 'hl':
     valorUnidadLitro = ingresaValor * 100
-} else if (eligeUnidad === 'dal') {
+    break
+  case 'dal':
     valorUnidadLitro = ingresaValor * 10
-} else if (eligeUnidad === 'l') {
+    break
+  case 'l':
     valorUnidadLitro = ingresaValor
-} else if (eligeUnidad === 'dl') {
+    break
+  case 'dl':
     valorUnidadLitro = ingresaValor * 0.1
-} else if (eligeUnidad === 'cl') {
+    break
+  case 'cl':
     valorUnidadLitro = ingresaValor * 0.01
-} else if (eligeUnidad === 'ml') {
+    break
+  case 'ml':
     valorUnidadLitro = ingresaValor * 0.001
-}
- else {
-    alert("Unidad inválida. Usá: kl, hl, dal, l, dl, cl, ml.")
+    break
+
+  default:
+    alert("Ingresá la unidad de medida: kl, hl, dal, l, dl, cl o ml.")
 }
 
+let resultadoConversion = 
+        'Equivalencias del valor ingresado:\n' +
 
-if (unidadEnLitro) {
-    let resultadoConversion = 
-        "Equivalencias del valor ingresado:\n" +
-        `${valorUnidadLitro / 1000} kilolitro\n` +
-        `${valorUnidadLitro / 100} hectolitro\n` +
-        `${valorUnidadLitro / 10} decalitro\n` +
-        `${valorUnidadLitro} litro\n` +
-        `${valorUnidadLitro * 10} decilitro\n` +
-        `${valorUnidadLitro * 100} centilitro\n` +
+        `${valorUnidadLitro / 1000} kilolitros\n` +
+        `${valorUnidadLitro / 100} hectolitros\n` +
+        `${valorUnidadLitro / 10} decalitros\n` +
+        `${valorUnidadLitro} litros\n` +
+        `${valorUnidadLitro * 10} decilitros\n` +
+        `${valorUnidadLitro * 100} centilitros\n` +
         `${valorUnidadLitro * 1000} mililitro`
 
     alert(resultadoConversion)
-}
