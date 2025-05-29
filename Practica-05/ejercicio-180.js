1516948af729e0fe4baee00580b5d7146795be00
CeciliaSalinas <salinascecilia.mdl@gmail.com>
2025-05-29 18:41:23 -0300

    .

--- Practica-05/ejercicio-180.js
+++ Practica-05/ejercicio-180.js
@@ -13,15 +13,16 @@ Si no está, informar que no se encuentra.
  */
 
 let ingresoElementos = prompt('Ingresar 30 numeros enteros')
+let arrayElmentos = ingresoElementos.split('').map(Number)
 let largoElementos = ingresoElementos.length
 
-let ingresoUnElemento = prompt('Ingresar un número entero:')
+
 
 if(largoElementos !== 30){
     alert(`Debés ingresar exactamente 30 caracteres. Ingresaste ${largoElementos}.`)
 }
 else{
-   let arrayElmentos = ingresoElementos.split('') 
+   let ingresoUnElemento = prompt('Ingresar un número entero:')
    let largoArray = arrayElmentos.length
 
    for( let i = 0; i < largoArray; i++ ){
@@ -35,7 +36,33 @@ function buscarCoincidencia(){
 }
 
 
+/* 
+let ingresoElementos = prompt('Ingresar 30 números enteros separados por espacio:');
+let arrayElementos = ingresoElementos.split(' ').map(Number);
 
+// Validar que haya exactamente 30 números y que todos sean válidos
+if (arrayElementos.length !== 30 || arrayElementos.some(num => isNaN(num))) {
+    alert(`Debés ingresar exactamente 30 números válidos separados por espacio.`);
+} else {
+    let ingresoUnElemento = parseInt(prompt('Ingresar un número entero a buscar:'));
+
+    if (isNaN(ingresoUnElemento)) {
+        alert('El número ingresado no es válido.');
+    } else {
+        let posicion = buscarCoincidencia(arrayElementos, ingresoUnElemento);
+
+        if (posicion !== -1) {
+            alert(`✅ El número ${ingresoUnElemento} se encuentra en la posición ${posicion} del arreglo.`);
+        } else {
+            alert(`❌ El número ${ingresoUnElemento} no está en el arreglo.`);
+        }
+    }
+}
+
+function buscarCoincidencia(arreglo, valorBuscado) {
+    return arreglo.indexOf(valorBuscado);
+}
+*/
 
 
 
