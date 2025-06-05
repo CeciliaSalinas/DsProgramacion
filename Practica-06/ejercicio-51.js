bab7d74bc7912a7bb6dbb6d136be4a1e93df2365
CeciliaSalinas <salinascecilia.mdl@gmail.com>
2025-06-05 17:51:00 -0300

    resolucion practica 6

---
+++ Practica-06/ejercicio-51.js
@@ -0,0 +1,42 @@
+console.log('Practica Javascript 06 - E51')
+
+function solucionEcuacion(a, b, c){
+    let discriminante = b*b - 4*a*c
+    if( discriminante < 0){
+        return null
+    }
+    if( discriminante == 0){
+        let x = -b/ (2*a)
+        return [x]
+    }
+    else{
+        if(discriminante > 0){
+            let x1 = (-b + Math.sqrt(discriminante  )) / (2*a)
+            let x2 = (-b - Math.sqrt(discriminante)) / (2*a)
+            return [x1, x2]
+        }
+    }
+    return discriminante
+}
+
+let a = parseInt(prompt('Ingresar el coeficiente a')) 
+let b = parseInt(prompt('Ingresar el coeficiente b')) 
+let c = parseInt(prompt('Ingresar el coeficiente c')) 
+
+if(a === 0){
+    alert('El coeficiente A no puede ser 0 para una ecuacion de segundo grado!')
+}
+else{
+    let soluciones = solucionEcuacion(a, b, c)
+    /* let solucionesReales = soluciones.length */
+    if(soluciones === null){
+        alert(`La ecuacion no tiene solución real.`)
+    }
+    else if(soluciones.length === 1){
+        alert(`La ecuacion tiene una sola solución real: x = ${soluciones[0]} `)
+    }
+    else{
+        alert(`La ecuación tiene dos soluciones reales: x1 = ${soluciones[0]} / x2 = ${soluciones[1]}`)
+    }
+}
+
