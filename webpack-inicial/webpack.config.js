6e7e1f03bd232b377ee7493cb8017966b212f959
CeciliaSalinas <salinascecilia.mdl@gmail.com>
2025-08-29 19:21:08 -0300

    configuracion webpack

---
+++ webpack-inicial/webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    /* mode: 'production', */
+    mode: 'development',
+    module:{
+        rules: [
+             {
+                test: /\.css$/,
+                exclude: /styles\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }, 
+            {
+                test: /styles\.css$/,
+                use:[
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {minimize: true}
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+            ignoreOrder: false
+        })
+    ]
+}
